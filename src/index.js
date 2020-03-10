93f649fd7e3536dda869fc52c5301f613c5fa191
Josh Gribbon <josh.gribbon@sphereco.com>
2020-03-10 11:25:14 -0400

    Track history and improve display

--- src/index.js
+++ src/index.js
@@ -1,21 +1,58 @@
-const charSet = ' ntesiroahdjglpufywqbkvmcxz1234567890\'\",.!?:;/@$%&#*()_~+-={}|^<>`[]\\ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const chars = 'ntesiroahdjglpufywqbkvmcxz'
 
 var app = new Vue({
   el: '#app',
+  data () {
+    return {
+      charSets: {
+        space: ' ',
+        lowercase: chars,
+        numbers: '0123456789',
+        symbols: '\'\",.!?:;/@$%&#*()_~+-={}|^<>`[]\\',
+        uppercase: chars.toUpperCase()
+      },
+      currentChar: undefined,
+      fullHistory: [],
+      futureChars: [],
+      peripheralCharLength: 6
+    }
+  },
   computed: {
     alphabet () {
-      return charSet.split('').map(value => {
-        return {
-          value
-        }
+      return Object.values(this.charSets).join('').split('').map(value => {
+        return { value }
       })
     },
-    currentChars () {
-      return this.alphabet.slice(0, 8)
+    history () {
+      return this.fullHistory.slice(1).slice(-this.peripheralCharLength)
+    }
+  },
+  mounted () {
+    document.addEventListener('keyup', this.handleKeypress);
+    this.currentChar = this.alphabet[0]
+    this.futureChars = this.alphabet.slice(0, this.peripheralCharLength)
+  },
+  methods: {
+    handleKeypress ({ key }) {
+      console.log({
+        a: this.alphabet,
+        key,
+        includes: this.alphabet.includes(key)
+      })
+      if (this.alphabet.some(c => c.value == key)) {
+        this.fullHistory.push({ value: key })
+        this.currentChar = this.futureChars.shift()
+        this.futureChars.push({
+          value: this.alphabet[Math.floor(Math.random() * this.alphabet.length)]
+        })
+      }
     }
   },
   filters: {
     charDisplay (char) {
+      if (typeof char === 'object' && char.value !== undefined) {
+        char = char.value
+      }
       const renameMap = {' ': '⎵'}
       return renameMap[char] || char
     }
